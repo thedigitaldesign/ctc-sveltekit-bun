b369a148ead9a118340870b0c37da10a2b0192bc
Joshua Christensen <joshua@codingwithjosh.com>
2022-08-01 21:57:04 -0500

    forgot to save

--- app/src/routes/activity/create.svelte
+++ app/src/routes/activity/create.svelte
@@ -6,8 +6,8 @@
   <div class="px-4 sm:px-6 lg:px-8">
     <div class="sm:flex sm:items-center">
       <div class="sm:flex-auto">
-        <h1 class="text-xl font-semibold text-gray-900">Add Counselor</h1>
-        <p class="mt-2 text-sm text-gray-700">Add a counselor that is part of your district.</p>
+        <h1 class="text-xl font-semibold text-gray-900">Add Activity</h1>
+        <p class="mt-2 text-sm text-gray-700">Add an activity that is part of your district.</p>
       </div>
     </div>
 
